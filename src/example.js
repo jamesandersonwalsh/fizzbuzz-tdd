23bad0febfbf527a5c0e52a05082239bfea5a519
James Walsh <jamesandersonwalsh@gmail.com>
2018-05-15 11:17:26 -0600

    Add example tests and implementations at each step

--- src/example.js
+++ src/example.js
@@ -1,4 +1,99 @@
 
-export default function fizzbuzz() {
-  throw new Error('Not implemented yet!')
+
+/*
+  Implementation at test 1:
+*/
+
+// export default function fizzbuzz(number) {
+//   return number
+// }
+
+/*
+  Implementation at test 2:
+*/
+
+// export default function fizzbuzz(number) {
+//   if (number % 3 === 0)
+//     return 'fizz'
+
+//   return number
+// }
+
+/*
+  Implementation at test 3:
+*/
+
+// export default function fizzbuzz(number) {
+//   if (number % 3 === 0)
+//     return 'fizz'
+//   else if (number % 5 === 0)
+//     return 'buzz'
+
+//   return number
+// }
+
+/*
+  Implementation at test 4:
+*/
+
+// export default function fizzbuzz(number) {
+//   if (number % 5 === 0 && number % 3 === 0)
+//     return 'fizzbuzz'
+//   if (number % 3 === 0)
+//     return 'fizz'
+//   else if (number % 5 === 0)
+//     return 'buzz'
+
+//   return number
+// }
+
+/*
+  NOTE: You're probably thinking, "But James! You TDD'ed and that last implementation looks gross!
+    Look at all that duplication.' Yup, you're totally right. Now lets practice some GREEN refactoring.
+    Since our cases are written for us, and we know we have code that already passes all of our suites,
+    we can refactor with confidence. As long as the tests still pass, we can clean this code however we please.
+    I'd personally opt for something like below:
+*/
+/*
+  Implementation when practicing green refacoring:
+*/
+
+export default function fizzbuzz(number) {
+  const isFizz = number % 3 === 0
+  const isBuzz = number % 5 === 0
+
+  if (isFizz && isBuzz)
+    return 'fizzbuzz'
+  else if (isFizz)
+    return 'fizz'
+  else if (isBuzz)
+    return 'buzz'
+
+  return number
 }
+
+/*
+  Extra credit edge case implementation:
+*/
+// export default function fizzbuzz(number) {
+//   validateParams(number)
+
+//   const isFizz = number % 3 === 0
+//   const isBuzz = number % 5 === 0
+
+//   if (isFizz && isBuzz)
+//     return 'fizzbuzz'
+//   else if (isFizz)
+//     return 'fizz'
+//   else if (isBuzz)
+//     return 'buzz'
+
+//   return number
+// }
+
+// function validateParams(arg) {
+//   const isInt = arg === parseInt(arg, 10)
+  
+//   if (!isInt)
+//     throw new Error('Parameter must be an integer')
+// }
--- src/example.test.js
+++ src/example.test.js
@@ -3,8 +3,54 @@ import { expect } from 'chai'
 import fizzbuzz from './example'
 
 describe('#fizzbuzz example', () => {
-  it ("returns the number", () => {
-    const actual = fizzbuzz(1)
-    expect(actual).to.eql(1)
+  describe('when the number is not a multiple of 3 or 5', () => {
+    it ("returns the number", () => {
+      const cases = [fizzbuzz(1), fizzbuzz(2), fizzbuzz(17)]
+
+      expect(cases[0]).to.eql(1)
+      expect(cases[1]).to.eql(2)
+      expect(cases[2]).to.eql(17)
+    })
+  })
+  
+  describe('when number is multiple of 3', () => {
+    it(`returns 'fizz'`, () => {
+      const cases = [fizzbuzz(3), fizzbuzz(6), fizzbuzz(9)]
+
+      cases.forEach(actual => {
+        expect(actual).to.eql('fizz')
+      })
+    })
+  })
+
+  describe('when number is multiple of 5', () => {
+    it(`returns 'buzz'`, () => {
+      const cases = [fizzbuzz(5), fizzbuzz(25), fizzbuzz(50)]
+
+      cases.forEach(actual => {
+        expect(actual).to.eql('buzz')
+      })
+    })
+  })
+
+  describe('when the number is a multiple of 3 & 5', () => {
+    it(`returns 'fizzbuzz'`, () => {
+      const cases = [fizzbuzz(15), fizzbuzz(30), fizzbuzz(75)]
+
+      cases.forEach(actual => {
+        expect(actual).to.eql('fizzbuzz')
+      })
+    })
+  })
+
+  // Extra Credit: Support for edge/exceptional cases:
+  describe.skip('when parameter is not a number', () => {
+    it('throws an Error', () => {
+      const errMessage = 'Parameter must be an integer'
+
+      expect(() => fizzbuzz('rick-and-morty')).to.throw(Error, errMessage)
+      expect(() => fizzbuzz(true)).to.throw(Error, errMessage)
+      expect(() => fizzbuzz({})).to.throw(Error, errMessage)
+    })
   })
 })
